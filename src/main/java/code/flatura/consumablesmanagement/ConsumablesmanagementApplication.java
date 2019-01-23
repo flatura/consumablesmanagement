7bd05911c79fdcb0057dadd11bec806ea129f14e
Dmitry Morozov <flatura@gmail.com>
2019-01-24 00:30:10 +0300

    Initial commit

---
+++ src/main/java/code/flatura/consumablesmanagement/ConsumablesmanagementApplication.java
@@ -0,0 +1,14 @@
+package code.flatura.consumablesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumablesmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumablesmanagementApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/code/flatura/consumablesmanagement/model/Cartridge.java
@@ -0,0 +1,15 @@
+package code.flatura.consumablesmanagement.model;
+
+import javafx.print.Printer;
+
+import java.util.List;
+
+/**
+ *  Класс картриджа
+ */
+public class Cartridge {
+    private String id;
+    private String name;
+    private List<Printer> compatiblePrintersList;
+
+}
